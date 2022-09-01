d8668c117821de0b340aa5abae5b840a38b89b7f
7thTransPort <85343430+7thTP@users.noreply.github.com>
2022-09-01 21:03:15 +0900

    Add files via upload

--- get_manaData.py
+++ get_manaData.py
@@ -1,5 +1,32 @@
 import mtgsdk
+import pandas as pd
+import numpy as np
 from mtgsdk import Card
 
-cards = Card.where(name = 'Bloodbraid Elf').all()
+
+def main():
+    TP = r'C:\temp\Deck - Gipsy Danger.txt'
+    print('input data path')
+    dl = get_kingyo_DeckList(TP)
+    set_ListToPD(dl)
+
+def get_kingyo_DeckList(TP):
+    f = open(TP, 'r', encoding='UTF-8')
+    dl = f.readlines()
+    f.close()
+    print('fech decklist')
+    return dl
+
+
+def set_ListToPD(dl):
+    dl2 =[]
+    for l in dl:
+        dl2.append(l.split(' ',1))
+    dfDeck = pd.DataFrame(dl2)
+    print('get deck')
+    return dfDeck
+if __name__ == "__main__":
+    main()
+
+cards = Card.where(name='Bloodbraid Elf').all()
 print(cards[0].mana_cost)
