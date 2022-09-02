7a8cbf81560155743edfc8fa12c410c2bcdcf17c
7thTP <musya0@yahoo.co.jp>
2022-09-02 19:43:17 +0900

    デッキ読み取り。からのマナコスト出力までできたけど、Artificer Classがバグる。

--- venv/Scripts/get_manaData.py
+++ venv/Scripts/get_manaData.py
@@ -8,25 +8,36 @@ def main():
     TP = r'C:\temp\Deck - Gipsy Danger.txt'
     print('input data path')
     dl = get_kingyo_DeckList(TP)
-    set_ListToPD(dl)
+    df_d=set_ListToPD(dl)
+    for index, item in df_d.iterrows():
+        manaCost = get_manaCost(item['name'])
+        print(item['name'],manaCost)
 
 def get_kingyo_DeckList(TP):
-    f = open(TP, 'r', encoding='UTF-8')
-    dl = f.readlines()
-    f.close()
-    print('fech decklist')
+    dl = []
+    with open(TP, "r",encoding='UTF-8') as file:
+        for i in file:
+            dl.append(i.rstrip('\n'))
     return dl
 
 
 def set_ListToPD(dl):
-    dl2 =[]
+    dl2 = []
     for l in dl:
-        dl2.append(l.split(' ',1))
-    dfDeck = pd.DataFrame(dl2)
+        dl2.append(l.split(' ', 1))
+    dfDeck = pd.DataFrame(dl2, columns=['number', 'name'])
     print('get deck')
     return dfDeck
+
+
+def get_manaCost(c_name):
+    if c_name:
+        cards = Card.where(name=c_name).all()
+        if cards[0].mana_cost:
+            return cards[0].mana_cost
+        else:
+            return '{0}'
+    else:
+        return 0
 if __name__ == "__main__":
     main()
-
-cards = Card.where(name='Bloodbraid Elf').all()
-print(cards[0].mana_cost)
