b2312f22e3f2fc706582fdc3bf33f747e28c3c48
7thTransPort <85343430+7thTP@users.noreply.github.com>
2022-10-05 23:04:18 +0900

    pandasでマナ表を作っている途中

--- get_manaData.py
+++ get_manaData.py
@@ -12,11 +12,23 @@ def main():
     TP = r'C:\temp\Deck - Gipsy Danger.txt'
     print('input data path')
     dl = get_kingyo_DeckList(TP)  # get Kingyo format data
-    df_d = set_ListToPD(dl)  # Reformat list
-
-    for index, item in df_d.iterrows():
-        manaCost = get_manaCost(item['name'],cards)
-        print(item['name'], manaCost)
+    df_d = listToPD(dl)  # Reformat list
+    deck = [nameToCardData(item['name'], cards) for index, item in df_d.iterrows()]  # Get deck card data
+    df_mana = pd.DataFrame(index=[], columns=['number'])
+    for card in deck:
+        print(card.name)
+        df_mana.loc[card.name] = 0
+        df_mana.at[card.name, 'number'] = df_d.at[df_d.index[df_d['name'] == card.name].tolist()[0],'number']
+        if ' ' in card.mana_cost:
+            mc_buf = card.mana_cost.split(' ')
+            # いったんSpritはあとで
+        else:
+            mana = card.mana_cost.replace('{', '').replace('}', '')
+            if mana in df_mana.columns.values:  # 既にある場合
+                df_mana.at[card.name, mana] = 1
+            else:
+                df_mana[mana] = 0
+                df_mana.at[card.name, mana] = 1
 
 
 def get_kingyo_DeckList(TP):
@@ -27,27 +39,26 @@ def get_kingyo_DeckList(TP):
     return dl
 
 
-def set_ListToPD(dl):  # MTG decklist format re-format ['number', 'name']
+def listToPD(dl):  # MTG decklist format re-format ['number', 'name']
     dl2 = []
     for l in dl:
         dl2.append(l.split(' ', 1))
     dfDeck = pd.DataFrame(dl2, columns=['number', 'name'])
-    print('get deck')
     return dfDeck
 
 
-def get_manaCost(c_name,cards):
+def nameToCardData(c_name, cards):
     if c_name:
         for card in cards:
-            if card.name == c_name:
-                if card.mana_cost:
-                    return card.mana_cost
-                else:
-                    return '{0}'
+            if '//' in card.name:  # Processing double-faced cards
+                sName = card.name.split(' // ')
+                if sName[0] == c_name or sName[1] == c_name:
+                    return card
+            elif card.name == c_name:  # Processing one-faced cards
+                return card
+        return 'not found'
     else:
         return 0
-
-
 
 
 if __name__ == "__main__":
